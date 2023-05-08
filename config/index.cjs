fc2fe2e5363475b2f1e324284466d158f27de427
chasegirls <790844730@qq.com>
2023-05-08 15:04:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'N96LV8ijiv8aIvqWfyLjsootcLqdywkgrkcLdmC9MiY',
+  CALLBACK_TEMPLATE_ID: 'JGrwTPh5C3fSn6y1cuAkzIZEllZOlAgtMWpei4r5d9I',
 
   CALLBACK_USERS: [
     {
