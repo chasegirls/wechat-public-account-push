55577a3d872c4d5d40b0893af856fbbb9017dc2b
chasegirls <790844730@qq.com>
2023-05-08 15:31:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '敏宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oSYf15rWK8SCS0z7v4gFhWqFfWPY',
+      id: 'oSYf15gmy-sYKQwp3AKbLHcwchpo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'N96LV8ijiv8aIvqWfyLjsootcLqdywkgrkcLdmC9MiY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
